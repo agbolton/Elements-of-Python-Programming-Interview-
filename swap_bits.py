853273b258317f65c108b1a14cdb7b7110a34800
Adam Bolton <adambolton13@gmail.com>
2023-08-15 15:38:32 -0700

    Added comments to program

--- swap_bits.py
+++ swap_bits.py
@@ -3,9 +3,10 @@ from test_framework import generic_test
 
 def swap_bits(x, i, j):
     # TODO - you fill in here.
+    # Check if Bits at i and J are different and needed to be switched
     if (x >> i) & 1 != (x >> j) & 1:
-        bit_mask = (1 << i) | (1 << j)
-        x ^= bit_mask
+        bit_mask = (1 << i) | (1 << j)  # Bit Mask - to Help Swap Creates a number with 1 in the swap position
+        x ^= bit_mask # XOR with Bit Mask - 1s are in swap position so if org was 1 -> 0
     return x
 
 
